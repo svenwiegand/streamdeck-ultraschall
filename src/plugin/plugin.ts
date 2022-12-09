3b32d968473ea122dd407724cdf1d63b00ad3f0b
Sven Wiegand <sven.wiegand@orgavision.com>
2022-12-09 15:43:55 +0100

    Implementing startup protocol

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,9 +1,33 @@
 import { Client } from "node-osc"
+import * as assert from "assert"
+import * as ws from "ws"
 
 console.log(new Date())
 process.argv.forEach(arg => console.log(arg))
 
+assert(process.argv[2] === "-port")
+const registration = {
+    port: parseInt(process.argv[3]),
+    uuid: process.argv[5],
+    event: process.argv[7],
+    info: JSON.parse(process.argv[9])
+}
+console.log(registration)
+
+const websocket = new ws.WebSocket(`ws://localhost:${registration.port}`)
+websocket.onopen = () => {
+    const payload = {
+        event: registration.event,
+        uuid: registration.uuid,
+    }
+    websocket.send(JSON.stringify(payload))
+}
+websocket.onmessage = event => {
+    console.log(event.data)
+}
+/*
 const osc = new Client("127.0.0.1", 8000)
 osc.send("t/play", () => {
     osc.close()
 })
+ */
