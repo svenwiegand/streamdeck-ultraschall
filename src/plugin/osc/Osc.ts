1ecc9616dc3ab9c8218ef7be077f559d2e129727
Sven Wiegand <sven.wiegand@orgavision.com>
2022-12-26 12:09:04 +0100

    Only reconnect if something really changed

--- src/plugin/osc/Osc.ts
+++ src/plugin/osc/Osc.ts
@@ -1,31 +1,45 @@
 import {ArgumentType, createUDPPort, Message, UDPPort} from "./typedOsc"
 import {EventEmitter} from "eventemitter3"
 
+interface Settings {
+    dawHost: string
+    dawPort: number
+    pluginHost: string
+    pluginPort: number
+}
+
 export class Osc {
     private emitter = new EventEmitter<string>()
     private port: UDPPort | undefined
+    private settings: Settings | undefined
 
     connect(dawHost: string, dawPort: number, pluginHost: string, pluginPort: number) {
-        this.close()
-        const port = createUDPPort({
-            remoteAddress: dawHost,
-            remotePort: dawPort,
-            localAddress: pluginHost,
-            localPort: pluginPort,
-        })
-        port.on("ready", () => {
-            this.port = port
-            console.log(`Listening for OSC on ${pluginHost}:${pluginPort}`)
-        })
-        port.on("error", (error) => {
-            console.error(`OSC error ${error.message}`)
-            this.port = undefined
-        })
-        port.on("message", (msg) => {
-            console.debug(msg)
-            this.emitter.emit(msg.address, msg)
-        })
-        port.open()
+        if (
+            !this.settings ||
+            this.settings.dawHost !== dawHost || this.settings.dawPort !== dawPort ||
+            this.settings.pluginHost !== pluginHost || this.settings.pluginPort !== pluginPort
+        ) {
+            this.close()
+            const port = createUDPPort({
+                remoteAddress: dawHost,
+                remotePort: dawPort,
+                localAddress: pluginHost,
+                localPort: pluginPort,
+            })
+            port.on("ready", () => {
+                this.port = port
+                console.log(`Listening for OSC on ${pluginHost}:${pluginPort}`)
+            })
+            port.on("error", (error) => {
+                console.error(`OSC error ${error.message}`)
+                this.port = undefined
+            })
+            port.on("message", (msg) => {
+                console.debug(msg)
+                this.emitter.emit(msg.address, msg)
+            })
+            port.open()
+        }
     }
 
     close() {
