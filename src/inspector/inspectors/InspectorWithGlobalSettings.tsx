5f3351eeab135651af0b5932e824378feb9c0953
Sven Wiegand <sven.wiegand@orgavision.com>
2022-12-28 08:53:05 +0100

    Completed global settings.

--- src/inspector/inspectors/InspectorWithGlobalSettings.tsx
+++ src/inspector/inspectors/InspectorWithGlobalSettings.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import {SDTextInput} from "react-streamdeck"
 import {GlobalSettings, globalSettingsDefault} from "common/actions/global"
 import {ActionInspector} from "streamdeck/inspector/ActionInspector"
 
@@ -22,23 +21,63 @@ export const InspectorWithGlobalSettings: React.FC<Props> = ({inspector, childre
         fetchGlobalSettings()
     }, [])
 
-    const setNumberFromString = (setter: (n: number) => void) =>
-        (event: React.ChangeEvent<HTMLInputElement>) => setter(Number(event.currentTarget.value))
+    const setNumberFromString = (setter: (n: number) => void) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        const n = parseInt(event.currentTarget.value)
+        if (!isNaN(n)) {
+            setter(n)
+        }
+    }
     const applySettings = () => {
         if (hostIp && sendPort && receivePort)
         inspector.setGlobalSettings({hostIp, sendPort, receivePort})
     }
     const inputs = hostIp && sendPort && receivePort ? (<>
-        <SDTextInput value={hostIp} label="Host IP" onChange={e => setHostIp(e.currentTarget.value)}/>
-        <SDTextInput value={sendPort.toString()} label="Send Port" onChange={setNumberFromString(setSendPort)}/>
-        <SDTextInput value={receivePort.toString()} label="Receive Port" onChange={setNumberFromString(setReceivePort)}/>
+        <div className="sdpi-item">
+            <div className="sdpi-item-label">Host IP</div>
+            <input
+                type="text"
+                value={hostIp}
+                onChange={e => setHostIp(e.currentTarget.value)}
+                className="sdpi-item-value"
+                required
+                pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+            />
+        </div>
+        <div className="sdpi-item">
+            <div className="sdpi-item-label">Send Port</div>
+            <input
+                type="text"
+                value={sendPort}
+                onChange={setNumberFromString(setSendPort)}
+                className="sdpi-item-value"
+                required
+                pattern="\d{1,5}"
+            />
+        </div>
+        <div className="sdpi-item">
+            <div className="sdpi-item-label">Receive Port</div>
+            <input
+                type="text"
+                value={receivePort}
+                onChange={setNumberFromString(setReceivePort)}
+                className="sdpi-item-value"
+                required
+                pattern="\d{1,5}"
+            />
+        </div>
+        <div className="sdpi-item">
+            <div className="sdpi-item-label empty"></div>
+            <a className="sdpi-item-value" href="https://github.com/svenwiegand/streamdeck-ultraschall" target="_blank">
+                See documentation for details
+            </a>
+        </div>
         <div className="sdpi-item">
             <button className="sdpi-item-value" onClick={applySettings}>Save</button>
         </div>
     </>) : <></>
     return (<>
         {children}
-        <details>
+        <details className="pointer">
             <summary>Global Settings</summary>
             {inputs}
         </details>
